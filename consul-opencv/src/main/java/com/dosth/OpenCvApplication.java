e86b6c0950194558fa248cd92d08764e327f52d8
guozhidong <zhidong.guo@cnbaosi.com>
2021-02-08 08:45:37 +0800

    opencv初始化

---
+++ consul-opencv/src/main/java/com/dosth/OpenCvApplication.java
@@ -0,0 +1,11 @@
+package com.dosth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenCvApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OpenCvApplication.class, args);
+	}
+}
